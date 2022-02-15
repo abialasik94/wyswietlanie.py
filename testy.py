be9cf2970abaed9285ae6ea576b9ce9511448bd1
abialasik <abialasik94@gmail.com>
2022-02-15 15:36:03 +0100

    Uzyskana ścieżka

--- testy.py
+++ testy.py
@@ -4,24 +4,32 @@ import re
 
 path = []
 def listIter(listFromDict):
-    for i in listFromDict:
+    for num, i in enumerate(listFromDict):
         if isinstance(i, list):
+            global path
+            path.append(num)
             listIter(i)
+            path = path[:-1]
         elif isinstance(i, dict):
+            path.append(num)
             dictIter(i)
+            path = path[:-1]
 
 def dictIter(d):
     for k, v in d.items():
         if isinstance(v, list):
-            listIter(v)
-        if isinstance(v, dict):
             global path
+            path.append(k)
+            listIter(v)
+            path = path[:-1]
+        if isinstance(v, dict):
             path.append(k)
             dictIter(v)
             path = path[:-1]
         else:
             if re.match(r"CIM.*Color", str(v)):
-                print("{0} : {1}".format(k, v))
+                a=0
+                #print("{0} : {1}".format(k, v))
                 print("Ścieżka: " + str(path))
 
 
@@ -30,3 +38,4 @@ with open('MIIP_Mapa_Emisji_UMWM_SR.mapx', encoding='utf-8') as data_file:
 
 dictIter(data)
 
+a=0
