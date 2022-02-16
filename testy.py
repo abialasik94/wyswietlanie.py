d253a9a888ff043f677498d1c68f59be99ba110d
abialasik <abialasik94@gmail.com>
2022-02-16 13:52:18 +0100

    wyszukuje po parametrach w ścieżce i regexie modelu kolorów, zmienia kolor, jeszcze nie wyszukuje po kolorze

--- testy.py
+++ testy.py
@@ -1,41 +1,58 @@
 # -*- coding: utf-8 -*-
 import json
 import re
+from functools import reduce  # forward compatibility for Python 3
+import operator
 
 path = []
-def listIter(listFromDict):
-    for num, i in enumerate(listFromDict):
-        if isinstance(i, list):
-            global path
-            path.append(num)
-            listIter(i)
-            path = path[:-1]
-        elif isinstance(i, dict):
-            path.append(num)
-            dictIter(i)
-            path = path[:-1]
+def zmienKolory(data, **kwargs):
+    if not all(elem in kwargs.keys() for elem in ['pathParameters', 'colorModel', 'colorValues']):
+        print('Nie został wprowadzony co najmniej 1 parametr z pathParameters, colorModel, colorValues')
+    else:
+        def getFromDict(dataDict, mapList):
+            return reduce(operator.getitem, mapList, dataDict)
+        def setInDict(dataDict, mapList, value):
+            changedValue = getFromDict(dataDict, mapList[:-1])[mapList[-1]] = value
+            return changedValue
+        def listIter(listFromDict, **kwargs):
+            for num, i in enumerate(listFromDict):
+                if isinstance(i, list):
+                    global path
+                    path.append(num)
+                    listIter(i, **kwargs)
+                    path = path[:-1]
+                elif isinstance(i, dict):
+                    path.append(num)
+                    dictIter(i, **kwargs)
+                    path = path[:-1]
 
-def dictIter(d):
-    for k, v in d.items():
-        if isinstance(v, list):
-            global path
-            path.append(k)
-            listIter(v)
-            path = path[:-1]
-        if isinstance(v, dict):
-            path.append(k)
-            dictIter(v)
-            path = path[:-1]
-        else:
-            if re.match(r"CIM.*Color", str(v)):
-                a=0
-                #print("{0} : {1}".format(k, v))
-                print("Ścieżka: " + str(path))
+        def dictIter(d, **kwargs ):
+            for k, v in d.items():
+                if isinstance(v, list):
+                    global path
+                    path.append(k)
+                    listIter(v, **kwargs)
+                    path = path[:-1]
+                if isinstance(v, dict):
+                    path.append(k)
+                    dictIter(v, **kwargs)
+                    path = path[:-1]
+                else:
+                    if re.match(kwargs['colorModel'], str(v)) and all(elem in path for elem in kwargs['pathParameters']):
+                        pathWithColorsToChange = path + ['values']
+                        setInDict(data, pathWithColorsToChange, kwargs['colorValues'])
+                        print('Zmieniono kolor dla ścieżki ' + str(path))
 
+        dictIter(data, **kwargs)
 
-with open('MIIP_Mapa_Emisji_UMWM_SR.mapx', encoding='utf-8') as data_file:
+nazwaPliku = "ABNowaKompozycjaBDOT10k.mapx"
+with open(nazwaPliku, encoding='utf-8') as data_file:
     data = json.load(data_file)
 
-dictIter(data)
+zmienKolory(data, pathParameters = ['layerDefinitions', 'renderer', 'symbol', 421],
+         colorModel = r"CIM.*Color", colorValues = [212, 505, 505, 0])
 
-a=0
+nowaNazwa = nazwaPliku[:-5] + '_zmieniony.mapx'
+changedFile = open(nowaNazwa, "w", encoding='utf-8')
+json.dump(data, changedFile, indent = 6)
+changedFile.close()
